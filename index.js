187dde7f48f0a217dcc940c11d041a484a7ed77f
Joost Holslag <info@joostholslag.nl>
2025-05-12 15:29:33 +0100

    update from Ian

--- index.js
+++ index.js
@@ -1,80 +1,111 @@
 // This collection is intented for use with Better Platfrom Sandbox https://sandbox.better.care
 // script checks for token existance and validity and fetches access token
-// set variables sandboxUsername and sandboxPassword in postman vault
+// set variables sandboxUsername and sandboxPassword in Environmet or API Collection/Varaibles
 // Environment variables are required: ehrApiUrl, openEhrApiUrl, terminologyUrl, demographicsUrl, getTokenUrl
+  
+//Requires authType Environeant variable to be set to
 
-function tokenExists(pm) {
-	var token = pm.environment.get('sandboxToken')
-	return (token != undefined) && (token != "")
+// Basic => Needs valid Username and Password
+// Token => Needs snadboxUserName, sandBoxPassword, sandboxToken(initially empty), getTokenUrl
+
+addAuthTokenString() //Needs to be a root-level of script
+
+const getBasicAuthString = () => {
+    const username = pm.environment.get('Username')
+    const password = pm.environment.get('Password')
+    const token = btoa(`${username}:${password}`)
+    return `Basic ${token}`
 }
 
-function getTokenPayload(token) {
-    // Postman does not support Buffer, use atob instead
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(
-        atob(base64)
-            .split('')
-            .map(function(c) {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            })
-            .join('')
-    );
-    return JSON.parse(jsonPayload);
+async function getTokenAuthString(){
+   if (!isTokenValid()) 
+        await getNewToken()
+   const token = pm.environment.get("sandboxToken")
+   return `Bearer ${token}`
 }
 
-function isTokenValid(pm) {
-	if (tokenExists()) {
-		var token = getTokenPayload(pm.environment.get('sandboxToken'))
-		return Math.round((new Date() / 1000)) < token.exp;
-	} else {
-		return false
-	}
+async function addAuthTokenString() {
+    let authTokenString = null;
+    const authType = pm.environment?.get("authType");
+    switch(authType) {
+     case "Token":
+        authTokenString = await getTokenAuthString()
+        break;  
+     case "Basic":
+        authTokenString = getBasicAuthString()
+        break;
+     default:
+        authTokenString = null
+    }
+
+    if(authTokenString) {
+        pm.request.headers.add({
+        key: "Authorization",
+        value: `${authTokenString}`
+    })
+    }
+    else
+      console.error('Invalid Auth type')
 }
 
-function getNewToken(pm) {
-
-	var username = pm.environment.get('sandboxUsername')
-	if (!username) {
-		console.log("Cannot fetch token. Username not set, please set varaible 'sandboxUsername'");
-		return null;
-	}
-
-	var password = pm.environment.get("sandboxPassword")
-	if (!password) {
-		console.log("Cannot fetch token. Password not set, please set variable 'sandboxPassword'")
-		return null;
-	}
-
-	pm.sendRequest({
-		url: pm.variables.replaceIn(pm.environment.get('getTokenUrl')),
-		method: 'POST',
-		header: {
-			'Content-Type': 'application/x-www-form-urlencoded', 'Accept': '*/*',
-			'Allow': 'application/json', 'Accept-Encoding': 'gzip, deflate, br'
-		},
-		body: {
-			mode: 'urlencoded',
-			urlencoded: [
-				{ key: 'grant_type', value: 'password' }, { key: 'client_id', value: 'portal' },
-				{ key: 'username', value: username }, { key: 'password', value: password }]
-		}
-	}, function (err, response) {
-		if (!err) {
-			var jsonData = response.json();
-			if (jsonData.error) {
-				console.error(jsonData.error)
-			} else {
-				pm.environment.set('sandboxToken', jsonData.access_token)
-				console.log("New access token fetched for: " + username)
-				console.log(getTokenPayload(pm.environment.get('sandboxToken')))
-			}
-		} else {
-			console.error(err)
-		}
-	});
+function tokenExists() {
+  var token = pm.environment.get('sandboxToken')
+  return (token != undefined) && (token != "") 
 }
 
-if (!isTokenValid()) {
-	getNewToken()
+function getTokenClaims(token) {
+    return JSON.parse(atob(token.split('.')[1]))
+}
+
+function isTokenValid() {
+  if (tokenExists()) {
+    var token = getTokenClaims(pm.environment.get('sandboxToken'))
+    return Math.round((new Date() / 1000)) < token.exp;
+  } else {
+    return false
+  }
+}
+
+async function getNewToken() {
+
+    var username = pm.variables.get('sandboxUsername')
+
+    if(!username) {
+        console.log("Cannot fetch token. Username not set, please set varaible 'sandboxUsername'");
+        return null;
+    }
+
+    var password = pm.variables.get('sandboxPassword') 
+    if(!password) {
+        console.log("Cannot fetch token. Password not set, please set variable 'sandboxPassword'")
+        return null;
+    }
+  
+  await pm.sendRequest({
+    url: pm.variables.replaceIn(pm.environment.get('getTokenUrl')),
+    method: 'POST',
+    header: {
+        'Content-Type': 'application/x-www-form-urlencoded','Accept': '*/*',
+        'Allow': 'application/json','Accept-Encoding': 'gzip, deflate, br'  },
+    body: {
+        mode: 'urlencoded',
+        urlencoded: [
+          { key:'grant_type', value:'password'}, { key:'client_id', value:'portal'},
+          { key:'username', value:username}, {key:'password', value:password}] }
+    }, function (err, response) {
+    
+    if (!err) {
+      var jsonData = response.json();
+      if (jsonData.error) {
+        console.error(jsonData.error)
+      } else {
+        pm.environment.set('sandboxToken', jsonData.access_token)
+        console.log("New access token fetched for: " + username)
+        console.log(getTokenClaims(pm.environment.get('sandboxToken')))
+        return jsonData.access_token
+      }
+    } else {
+      console.error(err)
+    }
+  });
 }
